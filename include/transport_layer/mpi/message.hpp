25d35b0e664606674061e1bd9de3c1c4d7eb19ba
Mauro Bianco <mbianco@cscs.ch>
2019-08-09 11:58:56 +0200

    keep fixing

--- include/transport_layer/mpi/message.hpp
+++ include/transport_layer/mpi/message.hpp
@@ -81,7 +81,7 @@ struct message
         : m_alloc{other.m_alloc}, m_capacity{other.m_capacity}, m_payload{nullptr}, m_size(other.m_size)
     {
         assert(m_size <= m_capacity); // sanity check
-        assert((m_capacity == 0) != (m_payload == nullptr)); // sanity check
+        assert((m_capacity == 0) == (other.m_payload == nullptr)); // sanity check
 
         if (m_capacity > 0) {
             m_payload = std::allocator_traits<Allocator>::allocate(m_alloc, m_capacity);
@@ -95,7 +95,7 @@ struct message
         : m_alloc{std::move(other.m_alloc)}, m_capacity{other.m_capacity}, m_payload{other.m_payload}, m_size(other.m_size)
     {
         assert(m_size <= m_capacity); // sanity check
-        assert((m_capacity == 0) != (m_payload == nullptr)); // sanity check
+        assert((m_capacity == 0) == (m_payload == nullptr)); // sanity check
 
         other.m_capacity = 0;
         other.m_payload = nullptr;
@@ -105,7 +105,7 @@ struct message
     ~message()
     {
         assert(m_size <= m_capacity); // sanity check
-        assert((m_capacity == 0) != (m_payload == nullptr)); // sanity check
+        assert((m_capacity == 0) == (m_payload == nullptr)); // sanity check
 
         if (m_payload)
             std::allocator_traits<Allocator>::deallocate(m_alloc, m_payload, m_capacity);
